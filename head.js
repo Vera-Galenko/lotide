38f54e5dd272ed1f01ade6af1721fad3b3a67d0b
Vera-Galenko <vera.galenko@icloud.com>
2020-09-14 22:50:50 +0000

    last

--- head.js
+++ head.js
@@ -1,15 +1,24 @@
+// const assertEqual = function (array, first) {
+//    if(array[0] === first){
+//        console.log(array[0])
+//    } if(array[0] !== first){
+//     console.log(array[0])}
+// };
+
+
+// assertEqual([5, 6, 7], 5);
+// assertEqual(["Hello", "Lighthouse", "Labs"], "Hello");
+// assertEqual([], "Hello");
+
+
 const assertEqual = function (array, first) {
-   if(array[0] === first){
-       console.log(array[0])
-   } if(array[0] !== first){
-    console.log(array[0])}
-};
+    if(array[0] === first){
+        console.log(array[0])
+    } if(array[0] !== first){
+    console.assert(array[0] === first, array[0])}
 
-
-assertEqual([5, 6, 7], 5);
-assertEqual(["Hello", "Lighthouse", "Labs"], "Hello");
-assertEqual([], "Hello");
-
-
-
-
+ };
+ 
+ assertEqual([5, 6, 7], 5);
+ assertEqual(["Hello", "Lighthouse", "Labs"], "Hello");
+ assertEqual([], "Hello");
