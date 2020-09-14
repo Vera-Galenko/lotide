15168ab7e15b800fc35d0b7c02957aba7b549e25
Vera-Galenko <vera.galenko@icloud.com>
2020-09-14 22:03:20 +0000

    new

--- assertEqual.js
+++ assertEqual.js
@@ -13,9 +13,9 @@ const assertEqual = function(actual, expected) {
   let tick = '✅✅✅';
   let cross = "🛑🛑🛑";
   if (actual === expected) {
-    console.log(tick + "Assertion Passed: " + actual + " === " + expected);
+    console.log(`${tick} Assertion Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
-    console.log(cross + "Assertion Passed: " + actual + " !== " + expected);
+    console.log(`${cross} Assertion Passed: ${actual} !== ${expected}`);
   }
 };
 
