38fb492f514e0c33721ca771545d0df3556fd7b5
Vera-Galenko <vera.galenko@icloud.com>
2020-09-14 21:50:42 +0000

    function

---
+++ assertEqual.js
@@ -0,0 +1,26 @@
+// const assertEqual = function(actual, expected) {
+// if (actual !== expected) {
+//     console.log("Assertion Failed: " + actual + " !== " + expected);
+// } else if (actual === expected) {
+//     console.log("Assertion Passed: " + actual + " === " + expected);
+// }
+// };
+
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+
+const assertEqual = function(actual, expected) {
+  let tick = '✅✅✅';
+  let cross = "🛑🛑🛑";
+  if (actual === expected) {
+    console.log(tick + "Assertion Passed: " + actual + " === " + expected);
+  } else if (actual !== expected) {
+    console.log(cross + "Assertion Passed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual(1, 1);
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("cat", "cat");
+assertEqual(4, 6);
+    
