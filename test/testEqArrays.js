fb24fbc302711c330b42f8dd353464b8cc7cecb3
Vera-Galenko <vera.galenko@icloud.com>
2020-09-19 21:55:10 +0000

    19/09/2020 5:55

--- test/testEqArrays.js
+++ test/testEqArrays.js
@@ -1,4 +1,14 @@
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual');
+// const eqArrays = require('../eqArrays');
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), true);
+
+
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), true);
+
+describe("#eqArrays", () => {
+  it("returns true for [1, 2, 3], [1, 2, 3]", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+});
