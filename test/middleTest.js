56a1b0a643424b32aed62727218abb906f16604b
Vera-Galenko <vera.galenko@icloud.com>
2020-09-20 00:20:47 +0000

    19/09/2020 8:20

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,18 @@
-const assertEqual = require('../assertEqual');
-const assertArraysEqual = require('../assertArraysEqual');
-const middle = require('../middle');
-assertEqual(assertArraysEqual(middle([1]), []), true);
-assertEqual(assertArraysEqual(middle([1, 2]), []), true);
-assertEqual(assertArraysEqual(middle([1, 2, 5, 7]), [2, 5]), true); 
-assertEqual(assertArraysEqual(middle([1, 2, 5, 7, 8, 9, 8, 9]), [ 7, 8 ]), true);
-assertEqual(assertArraysEqual(middle([1, 2, 5, 7, 6]), [ 5 ]), true);
+const assert = require('chai').assert;
+const middle   = require('../middle');
+
+describe("#middle", () => {
+  it("returns [2, 5] for [1, 2, 5, 7]", () => {
+    assert.deepEqual(middle([1, 2, 5, 7]), [2, 5]);
+  });
+  
+  it("returns [ 7, 8 ] for [1, 2, 5, 7, 8, 9, 8, 9]", () => {
+    assert.deepEqual(middle([1, 2, 5, 7, 8, 9, 8, 9]), [ 7, 8 ]);
+  });
+
+  it("returns [ ] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+
+});
